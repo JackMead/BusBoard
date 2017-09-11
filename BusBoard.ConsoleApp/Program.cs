13bf7361d5e6a652a93bb22a78c03d76dcb6e0cd
JackMead <Jack.Mead@softwire.com>
2017-09-11 10:34:27 +0100

    swap, tried the api, works

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,11 +8,20 @@ using System.Threading.Tasks;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var client = new RestClient("https://api-radon.tfl.gov.uk");
+
+            var request = new RestRequest("StopPoint/490008660N/Arrivals", Method.GET);
+            
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+            Console.WriteLine(content);
 
         }
     }
